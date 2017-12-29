470e1429356300031c2976831d4cf08446801ce5
shridharkaushik29 <shridharkaushik29@gmail.com>
2017-12-29 14:18:11 +0530

    Added hide support

--- jq-shanimate.js
+++ jq-shanimate.js
@@ -11,4 +11,15 @@
         }, duration * 1000)
     }
 
+    $.fn.shhide = function (classes) {
+        var element = this;
+        element.addClass(classes);
+        var duration = element.css('animation-duration');
+        duration = duration.replace("s", "");
+        duration = parseFloat(duration);
+        setTimeout(function () {
+            element.hide();
+        }, duration * 1000)
+    }
+
 })(jQuery)
